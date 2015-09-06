88dab027ebb0815cb12edfca96a1c5d788ea81bc
RomarioLopCaPC <romario_lopezc@hotmail.com>
2015-09-05 19:19:47 -0500

    Send Message y DisplayMessage agregados.

--- app/src/main/java/com/romarin/myfirstapp/MyActivity.java
+++ app/src/main/java/com/romarin/myfirstapp/MyActivity.java
@@ -1,11 +1,15 @@
 package com.romarin.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +38,16 @@ public class MyActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
